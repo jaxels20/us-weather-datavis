c401815aaead7bcc88f5969a538b5707ac1d4abd
jaxels20 <73275529+jaxels20@users.noreply.github.com>
2022-01-03 16:16:38 +0100

    dd

--- main.py
+++ main.py
@@ -7,10 +7,15 @@ import dash
 import dash_bootstrap_components as dbc
 import functions as func
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 
 
 city_list = ['NYC', 'HOU', 'CLT', 'CQT', 'IND', 'JAX', 'MDW', 'PHL', 'PHX', 'SEA']
+selected_cities1 = []
+selected_cities2 = []
+colors = ['aqua', 'darksalmon', 'grey', 'red', 'yellow']
+
 
 df_weather, df_placement = func.retrieve_data(city_list)
 
@@ -20,10 +25,12 @@ app = dash.Dash(__name__, suppress_callback_exceptions=True,
                 external_stylesheets=[dbc.themes.BOOTSTRAP]
                 )
 
-app.layout = html.Div([dbc.Row(
+app.layout = html.Div(
+    [html.H1('Visualisation of the weather in the US in the year 2015'), dbc.Row(
     dbc.Col(
-    dcc.Graph(id = 'map', clickData= ['customdata'], figure={'layout': {
-                                'height': 600,
+    dcc.Graph(id = 'map', figure = {'layout': {
+
+    'height': 600,
                                 'margin': {'l': 0, 'b': 0, 't': 0, 'r': 0}
                             }})
     , width=6)
@@ -47,7 +54,7 @@ def generate_map(clickdata):
                             lon='lon',
                             lat='lat',
                             zoom=3,
-                            center= {'lat' : 40.686288, 'lon': -98.24587245247825},
+                            center={'lat': 40.686288, 'lon': -98.24587245247825},
                             custom_data=['city']
                             )
     fig.update_layout(mapbox_style='open-street-map')
@@ -61,31 +68,104 @@ def generate_map(clickdata):
     Input(component_id='map', component_property='clickData')
 )
 def generate_subgraph(clickdata):
-    if type(clickdata) != list:
+    bool = True
+    if clickdata is not None:
         clicked_city = clickdata['points'][0]['customdata'][0]
-        dff = df_weather[df_weather['city'] == clicked_city]
+        if clicked_city in selected_cities1:
+            selected_cities1.remove(clicked_city)
+        else:
+            selected_cities1.append(clicked_city)
 
-        #fig = plt.bar(day_order, record_max_temps - record_min_temps, bottom=record_min_temps,
-        #    edgecolor='none', color='#C3BBA4', width=1)
-        fig = go.Figure()
+        dff = df_weather[df_weather['city'].isin(selected_cities1)]
 
-        #temp that day all time high:
-        fig.add_trace(go.Scatter(x=dff['date'], y=dff['record_min_temp'], mode='lines', line_color='indigo', fill=None))
-        fig.add_trace(go.Scatter(x=dff['date'], y=dff['record_max_temp'],mode='lines', line_color='indigo', fill='tonexty'))
+        final_fig = make_subplots(rows=len(selected_cities1), cols=1, shared_xaxes=False, shared_yaxes=True, subplot_titles=tuple(selected_cities1) )
+        final_fig.update_layout(height=450 * len(selected_cities1)
+        )
 
-        fig1 = go.Figure()
-        # fig.add_trace(
-        #     go.Scatter(x=dff['date'], y=dff['actual_mean_temp'], mode='lines', line_color='blue', fill='tonexty'))
-        #all time average temp that day:
-        fig.add_trace(go.Scatter(x=dff['date'], y=dff['average_min_temp'], mode='lines', line_color='blue', fill=None))
-        fig.add_trace(go.Scatter(x=dff['date'], y=dff['average_max_temp'],mode='lines', line_color='blue', fill='tonexty'))
-        return fig
+        for i, city in enumerate(selected_cities1, 1):
+            if bool:
+                #temp that day all time high/low:
+                final_fig.add_trace(go.Scatter(x=dff[dff['city'] == city]['date'], y=dff[dff['city'] == city]['record_min_temp'], mode='lines', line_color='indigo', fill=None, showlegend=False), col=1, row=i)
+                final_fig.add_trace(go.Scatter(x=dff[dff['city'] == city]['date'], y=dff[dff['city'] == city]['record_max_temp'],mode='lines', line_color='indigo', fill='tonexty', name= 'Span of min and max temperature ever recorded'), col=1, row=i)
+
+                final_fig.add_trace(
+                    go.Scatter(x=dff[dff['city'] == city]['date'], y=dff[dff['city'] == city]['actual_mean_temp'], mode='lines', line_color='black', name='Average temperature'), col=1, row=i)
+                #all time average temp that day:
+                final_fig.add_trace(go.Scatter(x=dff[dff['city'] == city]['date'], y=dff[dff['city'] == city]['average_min_temp'], mode='lines', line_color='blue', fill=None, showlegend=False), col=1, row=i)
+                final_fig.add_trace(go.Scatter(x=dff[dff['city'] == city]['date'], y=dff[dff['city'] == city]['average_max_temp'],mode='lines', line_color='blue', fill='tonexty', name='Span of average min and max temperature every year'), col=1, row=i)
+                bool = False
+            else:
+                # temp that day all time high/low:
+                final_fig.add_trace(
+                    go.Scatter(x=dff[dff['city'] == city]['date'], y=dff[dff['city'] == city]['record_min_temp'],
+                               mode='lines', line_color='indigo', fill=None, showlegend=False), col=1, row=i)
+                final_fig.add_trace(
+                    go.Scatter(x=dff[dff['city'] == city]['date'], y=dff[dff['city'] == city]['record_max_temp'],
+                               mode='lines', line_color='indigo', fill='tonexty',
+                               name='Span of min and max temperature ever recorded', showlegend=False), col=1, row=i)
+
+                final_fig.add_trace(
+                    go.Scatter(x=dff[dff['city'] == city]['date'], y=dff[dff['city'] == city]['actual_mean_temp'],
+                               mode='lines', line_color='black', name='Average temperature', showlegend=False), col=1, row=i)
+                # all time average temp that day:
+                final_fig.add_trace(
+                    go.Scatter(x=dff[dff['city'] == city]['date'], y=dff[dff['city'] == city]['average_min_temp'],
+                               mode='lines', line_color='blue', fill=None, showlegend=False), col=1, row=i)
+                final_fig.add_trace(
+                    go.Scatter(x=dff[dff['city'] == city]['date'], y=dff[dff['city'] == city]['average_max_temp'],
+                               mode='lines', line_color='blue', fill='tonexty',
+                               name='Span of average min and max temperature every year', showlegend=False), col=1, row=i)
+
+
+        final_fig.update_layout(legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1.02,
+            xanchor="right",
+            x=1
+        ))
+
+        #labels:
+        for i in range(1, len(selected_cities1)+1):
+            final_fig['layout'][f'yaxis{i}']['title'] = 'Temperature in fahrenheit'
+            final_fig['layout'][f'xaxis{i}']['title'] = 'Date'
+
+
+
+        return final_fig
     return px.scatter()
 
+@app.callback(
+    Output(component_id='subgraph2', component_property='figure'),
+    Input(component_id='map', component_property='clickData')
+)
+def generate_subgraph(clickdata):
+    if clickdata is not None:
+        clicked_city = clickdata['points'][0]['customdata'][0]
+        if clicked_city in selected_cities2:
+            selected_cities2.remove(clicked_city)
+        else:
+            selected_cities2.append(clicked_city)
+
+        dff = df_weather[df_weather['city'].isin(selected_cities2)]
+
+        fig = make_subplots(rows=2, cols=1, row_heights=[0.3, 0.7], shared_xaxes=True)
+
+        for i, city in enumerate(selected_cities2):
+            fig.add_trace(go.Box(x=dff[dff['city'] == city]['actual_mean_temp'], name=city, legendgroup='1', marker=dict(color=colors[i])),
+                          row=1, col=1)
+            hist_fig = go.Histogram(x=dff[dff['city'] == city]['actual_mean_temp'], name=city, marker= dict(opacity=0.70, color=colors[i]), legendgroup='1', showlegend=False)
+
+            fig.add_trace(hist_fig, row=2, col=1)
 
 
+        fig.layout['barmode'] = 'overlay'
+        fig['layout']['xaxis2']['title'] = 'Average temperature in fahrenheit'
+        fig['layout']['yaxis2']['title'] = 'Frequency'
+        fig.update_layout(title_text='Distribution of average temperature')
 
-
+        return fig
+    return px.histogram()
 
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(debug=True)
--- test.py
+++ test.py
@@ -6,11 +6,25 @@ import plotly.express as px
 import dash
 import dash_bootstrap_components as dbc
 import functions as func
+import seaborn as sns
+import matplotlib.pyplot as plt
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+
+
+
+
+
 
 city_list = ['NYC', 'HOU', 'CLT', 'CQT', 'IND', 'JAX', 'MDW', 'PHL', 'PHX', 'SEA']
 
 df_weather, df_placement = func.retrieve_data(city_list)
+pd.set_option('display.max_columns', None)
+df_HOU = df_weather[df_weather['city'] == 'HOU']
+df_NYC = df_weather[df_weather['city'] == 'NYC']
 
-print(df_weather)
-# dff = df_weather[df_weather['city'] == 'SEA']
-# print(dff)
+
+
+a = [1,2,3,4]
+
+print(tuple(a))
